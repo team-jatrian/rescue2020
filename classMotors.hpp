c3c97393dfa1adbdb62e92f849a28e6dcdfbc9d6
Jakob <jakob.benzing@arcor.de>
2019-10-26 21:28:02 +0200

    Update classMotors.hpp

--- classMotors.hpp
+++ classMotors.hpp
@@ -3,6 +3,11 @@ class motors {
     uint8_t ubLeftMappedSpeed;
     uint8_t ubRightMappedSpeed;
     uint8_t unOutputPins[4] = {AENBL, APHASE, BENBL, BPHASE};
+
+    //genaue assignments noch fixen
+    void setPhase(int8_t, int8_t);
+    uint8_t calcNewSpeed(int8_t, int8_t);
+
   public:
     uint8_t ubLeftSpeed;
     uint8_t ubRightSpeed;
@@ -14,29 +19,12 @@ class motors {
     uint8_t BENBL = 0;
     uint8_t BPHASE = 26;
 
+    uint8_t ubEngineSwitch;
+
 
     void initPins(){
       for(uint8_t i = 0; i <= (sizeof(nOutputPins) / sizeof(uint8_t)); i++){
-        pinMode(nOutputPins[i], OUTPUT);
-      }
-    }
-
-    void setPhase(int8_t nLeftSpeed, int8_t nRightSpeed){
-      if (nLeftSpeed < 0 && nRightSpeed < 0){
-        digitalWrite(APHASE, HIGH);
-        digitalWrite(BPHASE, LOW);
-      }
-      else if (nLeftSpeed < 0){
-        digitalWrite(APHASE, HIGH);
-        digitalWrite(BPHASE, HIGH);
-      }
-      else if (nRightSpeed < 0){
-        digitalWrite(APHASE, LOW);
-        digitalWrite(BPHASE, HIGH);
-      }
-      else{
-        digitalWrite(APHASE, LOW);
-        digitalWrite(BPHASE, HIGH);
+        pinMode(unOutputPins[i], OUTPUT);
       }
     }
 
@@ -44,8 +32,46 @@ class motors {
       ubLeftMappedSpeed = map(abs(nLeftSpeed), 0, 100, 0, ubMaxLeftSpeed);
       ubRightMappedSpeed = map(abs(nRightSpeed), 0, 100, 0, ubMaxRightSpeed);
       setPhase(nLeftSpeed, nRightSpeed);
-
-
-
+      if (ubEngineSwitch == 1){
+        analogWrite(AENBL, NewSpeed(ubLeftMappedSpeed, ubLeftSpeed))
+        analogWrite(BENBL, NewSpeed(ubRightMappedSpeed, ubRightSpeed))
+      }
+      else {
+        digitalWrite(AENBL, LOW);
+        digitalWrite(BENBL, LOW);
+      }
   }
 };
+
+motors::setPhase(int8_t nLeftSpeed, int8_t nRightSpeed){
+  if (nLeftSpeed < 0 && nRightSpeed < 0){
+    digitalWrite(APHASE, HIGH);
+    digitalWrite(BPHASE, LOW);
+    }
+  else if (nLeftSpeed < 0){
+    digitalWrite(APHASE, HIGH);
+    digitalWrite(BPHASE, HIGH);
+    }
+  else if (nRightSpeed < 0){
+    digitalWrite(APHASE, LOW);
+    digitalWrite(BPHASE, LOW);
+    }
+  else {
+    digitalWrite(APHASE, LOW);
+    digitalWrite(BPHASE, HIGH);
+    }
+  }
+
+motors::NewSpeed(int8_t nSetSpeed, int8_t &nCurrentSpeed){
+  if (nSetSpeed > &nCurrentSpeed){
+    nCurrentSpeed += 5;
+    return &nCurrentSpeed;
+  }
+  else if (nSetSpeed < &nCurrentSpeed){
+    &nCurrentSpeed -= 5;
+    return &nCurrentSpeed;
+  }
+  else {
+    return &nCurrentSpeed;
+  }
+}
